ab39c4c86664fa7ab083740b35f7fd266440876d
LuizOtavioTG <bc.luizo@gmail.com>
2025-09-12 20:48:45 -0300

    Fix: ajustando subscribe para a versão não deprecada.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro';
 
 @Component({
@@ -6,17 +7,23 @@ import { LivroService } from 'src/app/service/livro';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) { }
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI),
-      (error) => console.log(error)
-    )
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(),
+      error: erro => console.error(erro),
+      complete: () => console.log('Observable completado')
+    })
+
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
 
