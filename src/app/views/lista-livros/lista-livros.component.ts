5b21e02e9bd03b0830cb036ab21dec9404286682
LuizOtavioTG <bc.luizo@gmail.com>
2025-09-18 15:01:39 -0300

    Refactor: Adicionando operador distinctUntilChanged que emite somente os valores enviados pelo observable de origem se forem distintos em comparação com o último valor emitido pelo observable de resultado.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro';
@@ -10,23 +10,25 @@ import { LivroService } from 'src/app/service/livro';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent  {
+export class ListaLivrosComponent {
 
-  private readonly  PAUSA = 500;
+  private readonly PAUSA = 500;
   campoBusca = new FormControl();
 
   constructor(private service: LivroService) { }
 
-  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
-    tap(() => console.log('Fluxo inicial')),
-    debounceTime(this.PAUSA),
-    filter((valorDigitado)=> valorDigitado.length >= 3),
-    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map(items => this.livrosResultadoParaLivros(items))
-  )
-
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(this.PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      distinctUntilChanged(),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map((items) => this.livrosResultadoParaLivros(items))
+    )
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-    return items.map(item =>{
+    return items.map(item => {
       return new LivroVolumeInfo(item)
     })
   }
